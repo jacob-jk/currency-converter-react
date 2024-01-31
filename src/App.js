79ef17d83201b3f17a26428a0fef79e76b2fde7c
Kuba Karnath <jacob_jk@wp.pl>
2024-01-31 11:03:15 +0100

    structure of hooks changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Form from "./Form";
 import Container from "./Container";
 import Footer from "./Footer";
-import { currencies } from "./currencies";
+// import { currencies } from "./currencies";
 
 function App() {
-  const [currency, setCurrency] = useState(currencies[0]);
-
-  const changeCurrency = ({ target }) => setCurrency(target.name);
-
-  // const setRate = (currency) => {
-  //   const rate = currencies.find(({ name }) => name === currency).value;
-  // };
+  // const changeRate = (currency) =>
+  //   currencies.find(({ name, rate }) => {
+  //     if (name === currency) {
+  //       return rate;
+  //     }
+  //   });
 
   return (
     <Container>
-      <Form
-        changeCurrency={changeCurrency}
-        currency={currency}
-        // setRate={setRate}
-      />
+      <Form />
       <Footer />
     </Container>
   );
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,68 +1,82 @@
 import "./style.css";
+import { useState } from "react";
 import { currencies } from "../currencies";
 
-const Form = ({ currency, changeCurrency, setRate }) => (
-  <>
-    <p className="form__header">
-      <strong>Aktualny kurs walut</strong>
-    </p>
-    <form>
-      <fieldset className="form__fieldset">
-        <p>
-          <label>
-            <span className="form__positions">Waluta</span>
-            <select
-              className="form__box"
-              name="currency"
-              required
-              value={currency}
-              onChange={changeCurrency}
-            >
-              <option hidden>-brak-</option>
-              {currencies.map(({ id, name }) => (
-                <option key={id}>{name}</option>
-              ))}
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            <span className="form__positions">Kurs</span>
-            <input
-              className="form__box"
-              type="number"
-              name="rate"
-              step="any"
-              min="0"
-              required
-              disabled
-              onChange={setRate}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <span className="form__positions">Kwota</span>
-            <input
-              className="form__box"
-              type="number"
-              name="amount"
-              placeholder="Wpisz kwotę"
-              min="0"
-              step="any"
-              required
-            />
-          </label>
-        </p>
-      </fieldset>
-      <div>
-        <button className="form__button">Przelicz!</button>
-        <button className="form__button form__button--clearButton" type="reset">
-          Wyczyść!
-        </button>
-      </div>
-    </form>
-  </>
-);
+const Form = () => {
+  const [currency, setCurrency] = useState("");
+  const [rate, setRate] = useState("");
+
+  const changeCurrency = ({ target }) => setCurrency(target.value);
+  const changeRate = ({ target }) => setRate(target.value);
+  // const myRate = currencies.find(({ name }) => name === currency).value;
+
+  return (
+    <>
+      <p className="form__header">
+        <strong>Aktualny kurs walut</strong>
+      </p>
+      <form>
+        <fieldset className="form__fieldset">
+          <p>
+            <label>
+              <span className="form__positions">Waluta</span>
+              <select
+                className="form__box"
+                name="currency"
+                required
+                value={currency}
+                onChange={changeCurrency}
+              >
+                <option hidden>-brak-</option>
+                {currencies.map((currency) => (
+                  <option key={currency.id}>{currency.name}</option>
+                ))}
+              </select>
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="form__positions">Kurs</span>
+              <input
+                className="form__box"
+                type="number"
+                name="rate"
+                step="any"
+                min="0"
+                value={rate}
+                required
+                disabled
+                onChange={changeRate}
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="form__positions">Kwota</span>
+              <input
+                className="form__box"
+                type="number"
+                name="amount"
+                placeholder="Wpisz kwotę"
+                min="0"
+                step="any"
+                required
+              />
+            </label>
+          </p>
+        </fieldset>
+        <div>
+          <button className="form__button">Przelicz!</button>
+          <button
+            className="form__button form__button--clearButton"
+            type="reset"
+          >
+            Wyczyść!
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default Form;
