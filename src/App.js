50fed893c0cfc1eb3ab16082ab75aced00b2f723
Kuba Karnath <jacob_jk@wp.pl>
2024-02-06 17:08:03 +0100

    changing App.js order

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-// import { useState } from "react";
 import Form from "./Form";
 import Container from "./Container";
-// import { currencies } from "./currencies";
 
 function App() {
-  // const changeRate = (currency) =>
-  //   currencies.find(({ name, rate }) => {
-  //     if (name === currency) {
-  //       return rate;
-  //     }
-  //   });
-
   return (
     <Container>
       <Form />
