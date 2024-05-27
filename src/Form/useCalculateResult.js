ffaac6e29f3bec40a382f59d94b42d7d47d693b3
Kuba Karnath <jacob_jk@wp.pl>
2024-05-27 15:20:23 +0200

    editing objects and structure in calculating hook

--- src/Form/useCalculateResult.js
+++ src/Form/useCalculateResult.js
@@ -1,42 +1,36 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
+import { useGetData } from "./useGetData";
 
 export const useCalculateResult = () => {
   const [currency, setCurrency] = useState("");
   const [rate, setRate] = useState("");
   const [amount, setAmount] = useState("");
-  const [result, setResult] = useState("0,00");
-  const [exchangeData, setExchangeData] = useState("");
-
-  const getData = async () => {
-    const response = await axios.get("data.json");
-    setExchangeData(response.data.data);
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
+  const [result, setResult] = useState("");
+  const { exchangeData } = useGetData();
 
   const onCurrencyChange = ({ target }) => {
     setCurrency(target.value);
-    const selectedRate = Object.values(exchangeData).find(
-      ({ data }) => data === target.value
-    ).value;
-    setRate(selectedRate);
+    const selectedRate = exchangeData.data[target.value].value;
+    setRate(selectedRate.toFixed(2));
   };
 
   const onAmountChange = ({ target }) => setAmount(target.value);
 
   const calculateResult = () => {
-    const outcome = rate * amount;
-    setResult(outcome.toFixed(2));
+    const outcome = (rate * amount).toFixed(2);
+    setResult({
+      outcome,
+      currency,
+      amount,
+      date: new Date(exchangeData.actualDate).toLocaleDateString("pl-PL"),
+    });
   };
 
   const onReset = () => {
     setCurrency("");
     setRate("");
     setAmount("");
-    setResult("0,00");
+    setResult("");
   };
 
   const onFormSubmit = (event) => {
